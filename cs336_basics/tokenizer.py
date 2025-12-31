d78e39df485bf20709a42347da5e7bc014927e5b
Zhiwen Owen Jiang <ZhiwenOwenJiang@gmail.com>
2025-12-31 12:20:38 -0500

    debug for encode and decode

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -10,15 +10,16 @@ from dataclasses import dataclass
 class Node:
     sym: bytes
     next: Optional["Node"]
+    alive: bool = True
 
 
 class Tokenizer:
     PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
-    split_re = re.compile(PAT)
+    token_re = re.compile(PAT)
 
     def __init__(self, vocab: dict[int, bytes], merges: list[tuple[bytes, bytes]], special_tokens: list[str] | None = None):
         self.merges = merges
-        self.token_re = re.compile("|".join(map(re.escape, special_tokens)))
+        self.split_re = re.compile("|".join(map(re.escape, special_tokens)))
         self.idx2vocab = vocab
         self.vocab2idx = {v: k for k, v in vocab.items()}
 
@@ -47,38 +48,41 @@ class Tokenizer:
 
     def encode_one(self, word: bytes):
         byte2node = defaultdict(set)
+        all_nodes = list()
         prev = None
         head = None
-        for token_byte in word:
+        for i, x in enumerate(word):
+            token_byte = chr(x).encode()
             node = Node(sym=token_byte, next=None)
             if prev is not None:
                 prev.next = node
             else:
                 head = node
             prev = node
-            byte2node[token_byte].add(node)
+            byte2node[token_byte].add(i)
+            all_nodes.append(node)
 
         for merge in self.merges:
             if merge[0] in byte2node and merge[1] in byte2node:
-                nodes = list(byte2node.get(merge[0], ()))
-                for node in nodes:
-                    if node.next and node.next.sym == merge[1]:
+                node_idxs = list(byte2node.get(merge[0], ()))
+                for node_idx in node_idxs:
+                    node = all_nodes[node_idx]
+                    next_node = node.next
+                    if node.alive and next_node and next_node.alive and next_node.sym == merge[1]:
+                        next_node.alive = False
                         node.sym = merge[0] + merge[1]
-                        byte2node[merge[0]].discard(node)
-                        byte2node[merge[1]].discard(node.next)
-                        if node.next.next:
-                            node.next = node.next.next
+                        if next_node.next:
+                            node.next = next_node.next
                         else:
                             node.next = None
-                        byte2node[node.sym].add(node)
-                if len(byte2node[merge[0]]) == 0:
-                    del byte2node[merge[0]]
-                if len(byte2node[merge[1]]) == 0:
-                    del byte2node[merge[1]]
+                        byte2node[node.sym].add(node_idx)
+                        byte2node[merge[0]].discard(node_idx)
+                        if len(byte2node[merge[0]]) == 0:
+                            del byte2node[merge[0]]
                     
         output = list()
         curr = head
-        while curr:
+        while curr and curr.alive:
             output.append(self.vocab2idx[curr.sym])
             curr = curr.next
 
@@ -94,3 +98,15 @@ class Tokenizer:
             string += self.idx2vocab[id].decode()
 
         return string
+
+
+if __name__ == '__main__':
+    string = 'the cat ate'
+    vocab = {0: b' ', 1: b'a', 2: b'c', 3: b'e', 4: b'h', 5: b't', 6: b'th', 7: b' c', 8: b' a', 9: b'the', 10: b' at'}
+    merges = [(b't', b'h'), (b' ', b'c'), (b' ', b'a'), (b'th', b'e'), (b' a', b't')]
+    special_tokens = ["<|endoftext|>"]
+    
+    tokenizer = Tokenizer(vocab, merges, special_tokens)
+    encoding = tokenizer.encode(string)
+    decoding = tokenizer.decode(encoding)
+    
