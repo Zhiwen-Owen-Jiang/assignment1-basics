b47067a564600af3682a41091933e4643ab14e8a
Zhiwen Owen Jiang <ZhiwenOwenJiang@gmail.com>
2025-12-30 19:22:01 -0500

    merges pass the test

--- cs336_basics/bpe.py
+++ cs336_basics/bpe.py
@@ -1,7 +1,11 @@
 import regex as re
 import os
-from typing import BinaryIO
+from typing import BinaryIO, Optional
+from dataclasses import dataclass
 from collections import defaultdict
+
+
+Pair = tuple[bytes, bytes]
 
 
 def find_chunk_boundaries(
@@ -51,6 +55,20 @@ def find_chunk_boundaries(
     return sorted(set(chunk_boundaries))
 
 
+@dataclass
+class Node:
+    sym: bytes
+    prev: Optional[int]
+    next: Optional[int]
+    alive: bool = True
+
+
+@dataclass
+class Word:
+    head: Optional[int]
+    freq: int
+
+
 class BPE:
     PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
 
@@ -58,9 +76,12 @@ class BPE:
         self.input_path = input_path
         self.vocab_size = vocab_size
         self.special_tokens = special_tokens
-        self.pre_token_count = defaultdict(int)
-        self.adjcent_bytes_count = defaultdict(int)
-        self.count_adjcent_bytes = defaultdict(set)
+        self.word2freq = defaultdict(int)
+        self.pair2count = defaultdict(int)
+        self.count2pairs = defaultdict(set)
+        self.pair2occ = defaultdict(set)
+        self.nodes = list()
+        self.words = list()
 
     def train_bpe(self, desired_num_chunks=100, num_processes=4) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
         if "<|endoftext|>" not in self.special_tokens:
@@ -85,14 +106,24 @@ class BPE:
                     # pre-tokenization
                     for token in re.finditer(self.PAT, chunk):
                         token_bytes = token.group().encode()
-                        self.pre_token_count[token_bytes] += 1
-                
-            for vocab, count in self.pre_token_count.items():
-                for i in range(len(vocab) - 1):
-                    self.adjcent_bytes_count[(vocab[i:i+1], vocab[i+1:i+2])] += count
+                        self.word2freq[token_bytes] += 1
+            
+            # build the linked list for each word token as bytes
+            for word, freq in self.word2freq.items():
+                head = self.build_word(word)
+                self.words.append(Word(head=head, freq=freq))
 
-            for token, count in self.adjcent_bytes_count.items():
-                self.count_adjcent_bytes[count].add(token)
+            # pair occurrence index and pair2count
+            for word_idx, (word, freq) in enumerate(self.word2freq.items()):
+                start_idx = self.words[word_idx].head
+                for i in range(len(word) - 1):
+                    node_idx = start_idx + i
+                    self.pair2occ[(word[i:i+1], word[i+1:i+2])].add((word_idx, node_idx))
+                    self.pair2count[(word[i:i+1], word[i+1:i+2])] += freq
+
+            # count2pairs
+            for pair, count in self.pair2count.items():
+                self.count2pairs[count].add(pair)
                 
         merges, merged = self.bpe()
         vocab_list = [x.encode() for x in self.special_tokens] + [bytes(x) for x in range(256)] + merged
@@ -100,55 +131,142 @@ class BPE:
         
         return vocab, merges
 
+    def new_node(self, sym: bytes, prev: Optional[int]) -> int:
+        idx = len(self.nodes)
+        self.nodes.append(Node(sym=sym, prev=prev, next=None))
+        if prev is not None:
+            self.nodes[prev].next = idx
+        return idx
+
+    def build_word(self, tok: bytes) -> Optional[int]:
+        """
+        Build linked-list nodes for this token.
+        Returns head node index.
+
+        """
+        if not tok:
+            return None
+        
+        bts = list(tok)
+        head = None
+        prev = None
+
+        for b in bts:
+            sym = bytes([b])
+            cur = self.new_node(sym, prev)
+            if head is None:
+                head = cur
+            prev = cur
+
+        return head
+
+    def bucket_move_pair(self, p: Pair, newc: int) -> None:
+        """
+        If pair exists, remove the pair from count2pairs and update
+        If pair does not exist, directly update
+        
+        """
+        oldc = self.pair2count.get(p)
+
+        # If it existed before, remove it from the old bucket
+        if oldc is not None:
+            s = self.count2pairs.get(oldc)
+            if s is not None:
+                s.discard(p)
+                if not s:
+                    del self.count2pairs[oldc]
+
+        # If new count <= 0, delete it completely
+        if newc <= 0:
+            if oldc is not None:
+                del self.pair2count[p]
+            return
+        
+        self.pair2count[p] = newc
+        self.count2pairs[newc].add(p)
+
+    def add_to_pair(self, p: Pair, delta: int):
+        """
+        Add a new pair or existing pair with change
+        
+        """
+        if delta == 0:
+            return
+        oldc = self.pair2count.get(p, 0)
+        self.bucket_move_pair(p, oldc + delta)
+
+    def merge_once(self, A: bytes, B: bytes) -> None:
+        key = (A, B)
+        occs = list(self.pair2occ.get(key, ()))
+
+        for word_idx, node_idx in occs:
+            # extract all nodes
+            node1 = self.nodes[node_idx] # left node
+            if not node1 or not node1.alive or node1.sym != A:
+                continue
+            node2 = self.nodes[node1.next] if node1.next is not None else None # right node
+            if not node2 or not node2.alive or node2.sym != B:
+                continue    
+            pre_node = self.nodes[node1.prev] if node1.prev is not None else None # pre of left node
+            next_node = self.nodes[node2.next] if (node2 and node2.next is not None) else None # next of right node
+            
+            # update right side
+            if node2.sym == B:
+                node1.sym = A + B # merge (A, B) to one symbol
+                node2.alive = False
+                if next_node and next_node.alive:
+                    node2_idx = node1.next
+                    self.pair2occ[(A + B, next_node.sym)].add((word_idx, node_idx)) # if the next of right exists, create a new occ
+                    self.pair2occ[(B, next_node.sym)].discard((word_idx, node2_idx)) # remove the original occ
+                    self.add_to_pair((A + B, next_node.sym), self.words[word_idx].freq)
+                    self.add_to_pair((B, next_node.sym), -self.words[word_idx].freq)
+                    next_node.prev = node_idx # connect the next node to node1
+                node1.next = node2.next # connect the node1 to the next node
+
+            # update left side
+            if pre_node and pre_node.alive:
+                self.pair2occ[(pre_node.sym, A + B)].add((word_idx, node1.prev)) # if the pre of left exists, create a new occ
+                self.pair2occ[(pre_node.sym, A)].discard((word_idx, node1.prev)) # remove the original occ
+                self.add_to_pair((pre_node.sym, A + B), self.words[word_idx].freq)
+                self.add_to_pair((pre_node.sym, A), -self.words[word_idx].freq)
+
+            # update pair (A, B)
+            self.add_to_pair(key, -self.words[word_idx].freq)
+            self.pair2occ[key].discard((word_idx, node_idx))
+            s = self.pair2occ.get(key)
+            if s is not None and not s:
+                del self.pair2occ[key]
+
     def bpe(self,):
         merges = list()
         merged = list()
         vocab_size = self.vocab_size - len(self.special_tokens) - 256
 
         while len(merges) < vocab_size:
-            max_count = max(self.count_adjcent_bytes.keys())
-            bytes_to_merge = max(self.count_adjcent_bytes[max_count])
-            merges.append(bytes_to_merge)
-            merged_token = bytes_to_merge[0] + bytes_to_merge[1]
+            if not self.count2pairs:
+                break
+            max_count = max(self.count2pairs)
+            bucket = self.count2pairs[max_count]
+            if not bucket:
+                del self.count2pairs[max_count]
+                continue
+
+            pair = max(bucket)
+            if pair not in self.pair2occ:   # no occurrences left
+                bucket.discard(pair)
+                if not bucket: del self.count2pairs[max_count]
+                continue
+
+            merges.append(pair)
+            merged_token = pair[0] + pair[1]
             merged.append(merged_token)
-
-            for pre_token, count in self.pre_token_count.items():
-                for i in range(len(pre_token) - len(merged_token) + 1):
-                    if merged_token == pre_token[i:i+len(merged_token)]:
-                        if i > 0:
-                            self.add((pre_token[i-1:i], merged_token), count)
-                            for j in range(1, len(merged_token)):
-                                # for k in range(i):
-                                    self.remove((pre_token[i-1:i], merged_token[:j]), count)      
-
-                        if i + len(merged_token) < len(pre_token):
-                            self.add((merged_token, pre_token[i+len(merged_token):i+len(merged_token)+1]), count)
-                            for j in range(1, len(merged_token)):
-                                # for k in range(i+len(merged_token)+1, len(pre_token)+1):
-                                    self.remove((merged_token[j:], pre_token[i+len(merged_token):i+len(merged_token)+1]), count)
-                        
-                        self.remove(bytes_to_merge, count)
+            self.merge_once(*pair)
 
         return merges, merged
 
-    def add(self, token, count):
-        self.adjcent_bytes_count[token] += count 
-        self.count_adjcent_bytes[self.adjcent_bytes_count[token]].add(token)
-
-    def remove(self, token, count):
-        if token in self.adjcent_bytes_count:
-            self.count_adjcent_bytes[self.adjcent_bytes_count[token]].remove(token)
-            if len(self.count_adjcent_bytes[self.adjcent_bytes_count[token]]) == 0:
-                del self.count_adjcent_bytes[self.adjcent_bytes_count[token]]
-            self.adjcent_bytes_count[token] -= count
-            if self.adjcent_bytes_count[token] == 0:
-                del self.adjcent_bytes_count[token]
-            else:
-                self.count_adjcent_bytes[self.adjcent_bytes_count[token]].add(token)
-
 
 if __name__ == '__main__':
-    # bpe1 = BPE("/work/users/o/w/owenjf/stanford_cs336/data/toy_example.txt", 256+1+6, ["<|endoftext|>"])
+    # bpe1 = BPE("/work/users/o/w/owenjf/stanford_cs336/data/simple_case1.txt", 256+1+3, ["<|endoftext|>"])
     bpe1 = BPE("/work/users/o/w/owenjf/stanford_cs336/assignment1-basics/tests/fixtures/corpus.en", 500, ["<|endoftext|>"])
     vocab, merges = bpe1.train_bpe()
     vocab, merges
