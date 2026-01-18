a8cb43aeb72091c5d82a6305dd1254cf927bfad5
Zhiwen Owen Jiang <ZhiwenOwenJiang@gmail.com>
2026-01-17 19:16:10 -0500

    rope module

--- cs336_basics/transformer.py
+++ cs336_basics/transformer.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from einops import rearrange
 
 
 class Linear(nn.Module):
@@ -55,3 +56,44 @@ class SwiGLU(nn.Module):
         x = x1 * self.linear3(x)
         x = self.linear2(x)
         return x
+    
+
+class RotaryPositionalEmbedding(nn.Module):
+    def __init__(self, theta: float, d_k: int, max_seq_len: int, device=None, dtype=None):
+        super().__init__()
+        assert d_k % 2 == 0
+        self.theta = float(theta)
+        self.d_k = d_k
+        self.max_seq_len = max_seq_len
+
+        # inv_freq[j] = theta^(-2j/d)
+        j = torch.arange(0, d_k // 2, device=device, dtype=torch.float32)
+        inv_freq = self.theta ** (-2.0 * j / d_k)   # (B,)
+        self.register_buffer("inv_freq", inv_freq, persistent=False)
+
+    def forward(self, x, token_positions):
+        # x: (N,S,d), token_positions: (N,S)
+        N, S, d = x.shape
+        assert d == self.d_k and S <= self.max_seq_len
+
+        if token_positions.ndim == 1:          # (S,)
+            token_positions = token_positions.unsqueeze(0) # -> (1,S)
+
+        x_blk = x.reshape(N, S, d // 2, 2)  # (N,S,B,2) = [even, odd]
+
+        # angles: (N,S,B)
+        pos = token_positions.to(self.inv_freq.dtype)      # (N, S)
+        angles = rearrange(pos, "n s -> n s 1") * rearrange(self.inv_freq, "b -> 1 1 b")
+
+        c = torch.cos(angles)  # (N,S,B)
+        s = torch.sin(angles)  # (N,S,B)
+
+        x0 = x_blk[..., 0]     # (N,S,B)
+        x1 = x_blk[..., 1]     # (N,S,B)
+
+        # rotate
+        y0 = x0 * c - x1 * s
+        y1 = x0 * s + x1 * c
+
+        out = rearrange(torch.stack([y0, y1], dim=-1), "n s b r -> n s (b r)")
+        return out
